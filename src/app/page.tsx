bf41998032ad8cd15395f20164c3eff9d3055c63
Asad Ali <theeasadali@gmail.com>
2024-10-03 01:04:03 +0500

    Uploading NEXTJS assignment2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,33 @@
 import Image from "next/image";
-
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <>
+      {/* With TailwindCSS */}
+      <section className="my-0 mx-auto py-20 flex justify-between items-center w-10/12">
+      <h4 className="font-bold text-lg text-orange-500 gap-8 flex-row">With TailwindCSS</h4>
+        <div className="flex flex-col w-1/2 transition-all duration-300 hover:-translate-y-4 bg-red-700 rounded-2xl text-orange-50 p-20 text-2xl hover:shadow-xl">
+            <p>Salam! I'm,<br /> <h1 className="text-8xl font-bold text-white py-1">Asad Ali!</h1></p>
+            <span className="tracking-widest">A NextJs Developer</span>
+            <a href="http://linktr.ee/theeasadali" className="bg-orange-400 max-w-max py-2 px-5 my-5 rounded-lg transition-all duration-300 hover:bg-orange-500 " target="_blank" rel="noopener noreferrer">Let's connect!</a>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+        <div>
+          <Image src={`/images/Asad Ali NextJs Developer.png`} className="rounded-2xl border-[3px] border-red-700 transition-all duration-300 hover:-translate-y-4" width={400} height={400} alt="Asad Ali's NextJs Devloper Image"/>
+        </div>
+      </section>
+
+      {/* With CSS3 */}
+      <section className="container">
+      <h4 className="section-heading">With CSS3</h4>
+        <div className="content">
+          <p>Salam! I'm,<br /> <h1 className="name">Asad Ali!</h1></p>
+          <span className="role">A NextJs Developer</span>
+          <a href="http://linktr.ee/theeasadali" className="link-btn" target="_blank" rel="noopener noreferrer">Let's connect!</a>
+        </div>
+
+        <div>
+        <Image src={`/images/Asad Ali NextJs Developer.png`} className="profile-img" width={400} height={400} alt="Asad Ali's NextJs Devloper Image"/>
+        </div>
+      </section>
+    </>
   );
 }
